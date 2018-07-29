b26adc70d506bafdcc0bb8b6086487d118dde591
jasonkwan <690603179@qq.com>
2018-07-29 12:57:43 +0800

    补充了策略模式。简单易懂,代码少。

---
+++  strategy-pattern/src/main/java/com/gdufe/beahavior/WalkBehavior.java
@@ -0,0 +1,14 @@
+package com.gdufe.beahavior;
+
+/**
+ *  接口一般用于对动作的抽象,抽象类用于对对象的抽象
+ */
+
+/**
+ * @Author: GuanQihua
+ * @Description: WalkBehavior接口,故名思意是走路的方式
+ * @Date: 2018/7/29 12:52
+ */
+public interface WalkBehavior {
+    public void walk();
+}
--- singleton-pattern/src/main/java/com/gdufe/StaticCodeBlockSingleton.java
+++ singleton-pattern/src/main/java/com/gdufe/StaticCodeBlockSingleton.java
@@ -1,10 +1,5 @@
 package com.gdufe;
 
-/**
- * @Author: GuanQihua
- * @Description: 静态代码块，线程安全
- * @Date: 2018/7/29 11:03
- */
 public class StaticCodeBlockSingleton {
     private static Object MyObject;
 
@@ -15,5 +10,4 @@ public class StaticCodeBlockSingleton {
     public static Object getMyObject(){
         return MyObject;
     }
-
 }
